dc193789b5ed4a4f374b79dca8fd532a65507643
memikj-eldar-unilu <eldar.memikj.001@sudent.uni.lu>
2023-12-07 15:45:56 +0100

    new

--- SmartBus13Resource.py
+++ SmartBus13Resource.py
@@ -1,18 +1,8 @@
 from coapthon.resources.resource import Resource
 from pymongo import MongoClient
+from datetime import datetime, timedelta
 import threading
 import time
-
-def update_resource_value(resource):
-    while True:
-        # Update resource value here
-        resource.payload = get_new_value()  # Replace with your own method to get a new value
-        resource.updated_state()  # Notify observers about the change
-        time.sleep(60)  # Sleep for 60 seconds
-
-# Start a new thread that updates the resource value every 60 seconds
-threading.Thread(target=update_resource_value, args=(smartbus1,)).start()
-
 
 class SmartBusResource(Resource):
     def __init__(self, bus_id, coap_server=None):
@@ -22,6 +12,8 @@ class SmartBusResource(Resource):
         self.content_type = "text/plain"
         self.interface_type = "if1"
         self.bus_id = bus_id
+        self.timestamp = datetime.now()
+        self.vehicle_type = "Bus"  # Or get this value from MongoDB
 
         # MongoDB setup
         self.client = MongoClient('localhost', 27017)  # Update with your MongoDB details
@@ -29,9 +21,11 @@ class SmartBusResource(Resource):
         self.smartbus_collection = self.db['smartbus_collection']
 
     def render_GET(self, request):
+        # Check if the resource representation has expired
+        if datetime.now() - self.timestamp > timedelta(minutes=10):
+            return None  # Or however you want to handle expired resources
+
         # Retrieve data from MongoDB and update the payload
-        # For simplicity, let's assume a field 'location' in the MongoDB document
-        # that stores the current location of the SmartBus
         smartbus_data = self.smartbus_collection.find_one({"bus_id": self.bus_id})
         if smartbus_data:
             self.payload = "Location: {}".format(smartbus_data.get("location", "Unknown"))
@@ -39,14 +33,13 @@ class SmartBusResource(Resource):
 
     def render_PUT(self, request):
         # Update the location of the SmartBus in MongoDB based on the received payload
-        # For simplicity, let's assume the payload contains the new location
         new_location = request.payload
         self.smartbus_collection.update_one({"bus_id": self.bus_id}, {"$set": {"location": new_location}})
+        self.timestamp = datetime.now()  # Update timestamp
         return self
 
     def render_POST(self, request):
         # Create a new SmartBus resource in MongoDB
-        # For simplicity, let's assume the payload contains initial details
         initial_details = request.payload
         self.smartbus_collection.insert_one({"bus_id": self.bus_id, "details": initial_details, "location": "Unknown"})
         return self
@@ -59,3 +52,13 @@ class SmartBusResource(Resource):
 smartbus1 = SmartBusResource(1)
 smartbus2 = SmartBusResource(2)
 
+def update_resource_value(resource):
+    while True:
+        # Update resource value here
+        resource.payload = get_new_value()  # Replace with your own method to get a new value
+        resource.updated_state()  # Notify observers about the change
+        time.sleep(60)  # Sleep for 60 seconds
+
+# Start a new thread that updates the resource value every 60 seconds
+threading.Thread(target=update_resource_value, args=(smartbus1,)).start()
+threading.Thread(target=update_resource_value, args=(smartbus2,)).start()
