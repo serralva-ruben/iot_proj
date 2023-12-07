55f7b3e976103ba093952368193ce1ac0e5151c7
memikj-eldar-unilu <eldar.memikj.001@sudent.uni.lu>
2023-12-07 16:10:07 +0100

    new changes

--- CoApServerSetup.py
+++ CoApServerSetup.py
@@ -1,12 +1,13 @@
 from SmartBus13Resource import SmartBusResource
+from SmartCar1Resource import SmartCar1Resource
 from coapthon.server.coap import CoAP
-from coapthon.resources.resource import Resource
 
 class CoAPServer(CoAP):
     def __init__(self, host, port):
         CoAP.__init__(self, (host, port))
         self.add_resource('SmartBus1/', SmartBusResource(1))
         self.add_resource('SmartBus2/', SmartBusResource(2))
+        self.add_resource('SmartCar1/', SmartCar1Resource())
 
 server = CoAPServer("localhost", 5683)
 try:
